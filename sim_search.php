c4b6acd124325913bf4f5d10cd38ea6db22b87ac
Manish Jain <man_21jain@yahoo.com>
2022-04-14 15:21:12 +0400

    removing pagination

--- sim_search.php
+++ sim_search.php
@@ -1,11 +1,9 @@
 <?php
 echo $sim=$_GET["sim"];
 $cluster = Cassandra::cluster()
-//->withContactPoints('ec2-13-233-195-46.ap-south-1.compute.amazonaws.com')
 ->withContactPoints('172.16.1.28,172.16.1.182,172.16.1.181,172.16.1.25,172.16.1.185')
 ->withPort(9042)
 ->withCredentials("earthone", "XuWxgzpZ2rz")
-//->withCredentials("earthone", "earthone")
 ->build();
 $session = $cluster->connect('earthone');
 $result  = $session->executeAsync("SELECT * FROM trackers_by_imei where sim_number='".$sim."' ALLOW FILTERING");
