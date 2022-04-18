2abe8d1ddf117d4380eb72744ea38f398602bdc2
Manish Jain <man_21jain@yahoo.com>
2022-04-18 10:50:25 +0400

    cnagen connection

--- searchChassis.php
+++ searchChassis.php
@@ -1,5 +1,11 @@
 <?php
-require connProd.php;
+$cluster = Cassandra::cluster()
+->withContactPoints('172.16.1.28,172.16.1.182,172.16.1.181,172.16.1.25,172.16.1.185')
+->withPort(9042)
+->withCredentials("earthone", "XuWxgzpZ2rz")
+->build();
+$session = $cluster->connect('earthone');
+
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
