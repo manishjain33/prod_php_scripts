7ba1f1a32ca450c5769b45bb36b63880ba667ead
Manish Jain <man_21jain@yahoo.com>
2022-04-27 13:44:09 +0400

    adding the cassandra logic

--- sim_list.php
+++ sim_list.php
@@ -16,5 +16,20 @@ while ($trackerid = mysqli_fetch_assoc($result))
 {
   $tidData[]=$trackerid;
 }
-    print_r($tidData);
+mysqli_close($dbhandle);
+print_r($tidData);
+$cluster = Cassandra::cluster()
+//->withContactPoints('ec2-13-233-195-46.ap-south-1.compute.amazonaws.com')
+->withContactPoints('172.16.1.28,172.16.1.182,172.16.1.181,172.16.1.25,172.16.1.185')
+->withPort(9042)
+->withCredentials("earthone", "XuWxgzpZ2rz")
+->build();
+$session = $cluster->connect('earthone');
+for($i=0;$i>count($tidData);$i++){
+    $result  = $session->execute("SELECT * FROM trackers_by_trackerid where trackerid=".$tidData[$i]);
+    foreach ($result as $row) {
+        echo $row['imei']." / ".$row['trackerid']." / ".$row['sim_number'];
+    }
+}
+
 ?>
