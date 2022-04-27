884dea0a2118985306867af0d2c944a0abe04344
Manish Jain <man_21jain@yahoo.com>
2022-04-27 13:54:50 +0400

    correct for

--- sim_list.php
+++ sim_list.php
@@ -17,14 +17,14 @@ while ($trackerid = mysqli_fetch_assoc($result))
   $tidData[]=$trackerid;
 }
 mysqli_close($dbhandle);
-print_r($tidData[0]['tid']);
+//print_r($tidData[0]['tid']);
 $cluster = Cassandra::cluster()
 ->withContactPoints('172.16.1.28,172.16.1.182,172.16.1.181,172.16.1.25,172.16.1.185')
 ->withPort(9042)
 ->withCredentials("earthone", "XuWxgzpZ2rz")
 ->build();
 $session = $cluster->connect('earthone');
-for($i=0;$i>count($tidData);$i++){
+for($i=0;$i<count($tidData);$i++){
     echo "SELECT * FROM trackers_by_trackerid where trackerid=".$tidData[$i]['tid'];
     //echo "<br>";
     $result  = $session->execute("SELECT * FROM trackers_by_trackerid where trackerid=".$tidData[$i]['tid']);
