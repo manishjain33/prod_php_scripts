b2e0a585f710c170e46e963da232c559a89fca1e
Manish Jain <man_21jain@yahoo.com>
2022-05-24 09:39:59 +0400

    sim update

---
+++ sim_update.php
@@ -0,0 +1,13 @@
+<?php
+$simnum=$_GET['sno'];
+$cluster = Cassandra::cluster()
+->withContactPoints('172.16.1.28,172.16.1.182,172.16.1.181,172.16.1.25,172.16.1.185')
+->withPort(9042)
+->withCredentials("earthone", "XuWxgzpZ2rz")
+->build();
+$session = $cluster->connect('earthone');
+$result  = $session->execute("SELECT * FROM sim_cards WHERE (msisdn = $simnum) ALLOW FILTERING;");
+foreach ($result as $row) {
+    echo $row['company'];
+}
+?>
