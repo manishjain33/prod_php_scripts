5f636c2eca0dbf60a2823cd7d46ab01a3a762075
Manish Jain <man_21jain@yahoo.com>
2022-06-15 12:35:24 +0400

    trackers_by_userid st update

---
+++ st_tra_userid.php
@@ -0,0 +1,25 @@
+<?php
+include('connProd.php') ;
+$username = "dps";
+$password = "dps123";
+$hostname = "172.16.1.4";  
+$dbhandle = mysqli_connect($hostname, $username, $password) ;
+$selected = mysqli_select_db($dbhandle,'dubai') ;
+if($dbhandle->connect_errno > 0){
+  die('Unable to connect to database' . $dbhandle->connect_error);
+}
+$orgQuery = "select * from tid1";
+$orgQuery_result = mysqli_query($dbhandle, $orgQuery);
+while ($orgRow = mysqli_fetch_assoc($orgQuery_result))
+{
+  $trackers[]=$orgRow;
+}
+for($i=0;$i<=count($trackers);$i++){
+    $result  = $session->execute("SELECT * FROM trackers_by_trackerid WHERE (trackerid =".$trackers[$i]['tid']." );");
+    foreach ($result as $row) {
+        print_r($row);
+        echo"<br>";
+    }
+}
+
+?>
