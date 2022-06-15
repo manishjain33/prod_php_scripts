5560e1bf7fb553c2751f3313ff7e9ef054a081c6
Manish Jain <man_21jain@yahoo.com>
2022-06-15 10:44:40 +0400

    test print

--- duplicate_sim.php
+++ duplicate_sim.php
@@ -4,16 +4,18 @@ $username = "dps";
 $password = "dps123";
 $hostname = "172.16.1.4";  
 $dbhandle = mysqli_connect($hostname, $username, $password) ;
-$selected = mysqli_select_db($dbhandle,'gredenza') ;
+$selected = mysqli_select_db($dbhandle,'dubai') ;
 if($dbhandle->connect_errno > 0){
   die('Unable to connect to database' . $dbhandle->connect_error);
 }
-$orgQuery = "select * from ";
+$orgQuery = "select * from sim1";
 $orgQuery_result = mysqli_query($dbhandle, $orgQuery);
 while ($orgRow = mysqli_fetch_assoc($orgQuery_result))
 {
   $chassData[]=$orgRow;
 }
+print_r($chassData);
+echo "<br>";
 $result  = $session->execute("SELECT * FROM users_by_userid WHERE (user_type = 'vendor') ALLOW FILTERING;");
 foreach ($result as $row) {
     echo $row['company'];
