c0b1d7158612b8f1016c4211fb70aff3d77583d6
Manish Jain <man_21jain@yahoo.com>
2022-06-15 10:41:37 +0400

    mysql connection

--- duplicate_sim.php
+++ duplicate_sim.php
@@ -1,13 +1,22 @@
 <?php
 include('connProd.php') ;
-// $cluster = Cassandra::cluster()
-// ->withContactPoints('172.16.1.28,172.16.1.182,172.16.1.181,172.16.1.25,172.16.1.185')
-// ->withPort(9042)
-// ->withCredentials("earthone", "XuWxgzpZ2rz")
-// ->build();
-// $session = $cluster->connect('earthone');
+$username = "dps";
+$password = "dps123";
+$hostname = "172.16.1.4";  
+$dbhandle = mysqli_connect($hostname, $username, $password) ;
+$selected = mysqli_select_db($dbhandle,'gredenza') ;
+if($dbhandle->connect_errno > 0){
+  die('Unable to connect to database' . $dbhandle->connect_error);
+}
+$orgQuery = "select * from ";
+$orgQuery_result = mysqli_query($dbhandle, $orgQuery);
+while ($orgRow = mysqli_fetch_assoc($orgQuery_result))
+{
+  $chassData[]=$orgRow;
+}
 $result  = $session->execute("SELECT * FROM users_by_userid WHERE (user_type = 'vendor') ALLOW FILTERING;");
 foreach ($result as $row) {
     echo $row['company'];
 }
+
 ?>
