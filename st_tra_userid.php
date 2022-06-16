948ba34cc5f6f0dd4e310d5f4ed953c7b6db7568
Manish Jain <man_21jain@yahoo.com>
2022-06-16 14:27:11 +0400

    c

--- st_tra_userid.php
+++ st_tra_userid.php
@@ -18,11 +18,11 @@ while ($orgRow = mysqli_fetch_assoc($orgQuery_result))
 for($i=0;$i<=25;$i++){
     $result  = $session->execute("SELECT * FROM trackers_by_trackerid WHERE (trackerid =".$trackers[$i]['tid']." );");
     foreach ($result as $row) {
-        $data[]=json_decode($row['userid']);
+        $data=json_decode($row['userid']);
+        echo $data;
         echo"<br>";
         echo "count - ".count($row['userid']);
         echo"<br>";
     }
 }
-var_dump($data);
 ?>
