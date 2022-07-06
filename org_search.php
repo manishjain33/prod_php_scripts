6316e2b842a9dab71f32817026fb33f1e247b1e2
Manish Jain <man_21jain@yahoo.com>
2022-07-06 15:27:38 +0400

    add vedor

--- org_search.php
+++ org_search.php
@@ -3,6 +3,13 @@ include('connProd.php') ;
 $org=$_GET["org"];
 $result  = $session->execute("SELECT * FROM organizations where name='".$org."' ALLOW FILTERING");
 foreach ($result as $row) {
-    echo $row['id']." , ";
+    $orgid= $row['id'];
+    $vendorid=$row['vendorid'];
+    $vendorQ  = $session->execute("SELECT * FROM users_by_userid where userid='".$vendorid);
+    foreach ($vendorQ as $rowQ) {
+        $vendorname=$rowQ['company'];
+    }
+    $finaldata[]=array("orgid"=>$orgid,"vendor"=>$vendorid,"vendor_name"=>$vendorname);
 }
+print_r ($finaldata);
 ?>
