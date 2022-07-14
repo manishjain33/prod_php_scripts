4b7c3c33975a0a8b9af3b4293396510fc3eba215
Manish Jain <man_21jain@yahoo.com>
2022-07-14 14:39:53 +0400

    adding name nad email

--- org_search.php
+++ org_search.php
@@ -6,11 +6,13 @@ $result  = $session->execute("SELECT * FROM organizations where name='".$org."' 
 foreach ($result as $row) {
     $orgid= $row['id'];
     $vendorid=$row['vendorid'];
+    $email=$row['owner_email'];
+    $name=$row['owner_name'];
     $vendorQ  = $session->execute("SELECT * FROM users_by_userid where userid=".$vendorid);
     foreach ($vendorQ as $rowQ) {
         $vendorname=$rowQ['company'];
     }
-    $finaldata[]=array("orgid"=>$orgid,"vendor"=>$vendorid,"vendor_name"=>$vendorname);
+    $finaldata[]=array("orgid"=>$orgid,"Owner name"=>$name, "Owner email"=>$email, "vendor"=>$vendorid,"vendor_name"=>$vendorname);
 }
 echo json_encode ($finaldata);
 ?>
