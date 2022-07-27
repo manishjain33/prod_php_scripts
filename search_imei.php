8dc2c302590c839d229127ba9ccc1ee273151ff8
Manish Jain <man_21jain@yahoo.com>
2022-07-27 21:54:14 +0400

    corrected

--- search_imei.php
+++ search_imei.php
@@ -1,7 +1,6 @@
 <?php
 include('connProd.php') ;
-//$imei=$_GET['imei'];
-$imei=866907057592486;
+$imei=$_GET['imei'];
 $result  = $session->execute("SELECT * FROM trackers_by_imei where imei='".$imei."'");
 foreach ($result as $row) {
     $catagory=$row['foa_type'];
