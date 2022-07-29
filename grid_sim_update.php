4b4a92283bab2e7a76e8aedc28e227ee3c786b09
Manish Jain <man_21jain@yahoo.com>
2022-07-29 10:22:16 +0400

    final

--- grid_sim_update.php
+++ grid_sim_update.php
@@ -14,8 +14,8 @@ while ($simRow = mysqli_fetch_assoc($simQuery_result))
 {
   $simData[]=$simRow;
 }
-//for ($i=0;$i<=count($simData);$i++){
-for ($i=0;$i<=3;$i++){
+for ($i=0;$i<=count($simData);$i++){
+//for ($i=0;$i<=3;$i++){
     $result  = $session->execute("SELECT * FROM trackers_by_imei where imei='".$simData[$i]["imei"]."'");
     foreach ($result as $row) {
         $tid= $row['trackerid'];
@@ -78,6 +78,7 @@ for ($i=0;$i<=3;$i++){
     $err= curl_error($curl);
     curl_close($curl);
     echo $a." -- ";
+    echo $response."<br>";
     $rep=json_decode($response);
     if($rep->error_code!="unauthorized_access"){
         $update  = $session->execute("UPDATE sim_cards SET status = 'active' WHERE (iccid = '".$simData[$i]["ICCID"]."')");
