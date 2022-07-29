bb0946df3508cc970401fb4ba3ae18a946c654e2
Manish Jain <man_21jain@yahoo.com>
2022-07-29 10:19:27 +0400

    Etisalt integration

--- grid_sim_update.php
+++ grid_sim_update.php
@@ -76,11 +76,35 @@ for ($i=0;$i<=3;$i++){
 
     $response = curl_exec($curl);
     $err= curl_error($curl);
+    curl_close($curl);
     echo $a." -- ";
     $rep=json_decode($response);
     if($rep->error_code!="unauthorized_access"){
         $update  = $session->execute("UPDATE sim_cards SET status = 'active' WHERE (iccid = '".$simData[$i]["ICCID"]."')");
         echo "Status updated <br>";
+
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://restapi4.jasper.ae/rws/api/v1/devices/'.$simData[$i]["ICCID"],
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'PUT',
+            CURLOPT_POSTFIELDS =>'{
+                "status": "ACTIVATED"
+          }',
+            CURLOPT_HTTPHEADER => array(
+              'Authorization: Basic RU1jb2RlOjQ2YjFmYzQzLTFkNGMtNDg4NS1hZmI1LTRlNzYzZDFkOWUzNA==',
+              'Content-Type: application/json',
+              'Cookie: JSESSIONID=E2F56995FF38010A0DF3FCF34D5FB210; BIGipServer~Control_Center~POOL_POD4_RWS=!o/UypoTKTj6QWfZMDTqO4P+EVERACo/imLSyfq4eWtH+kiyNO7sSVZPrUsgMy+aszRRNhtpYHURjtFmC5B4gb2tk4b/LhpbaBHg0w013Jw+lwg=='
+            ),
+          ));
+        
+          $response = curl_exec($curl);
+          echo $response."<br>";
     }
     print_r($tid);
     echo "<br>";
