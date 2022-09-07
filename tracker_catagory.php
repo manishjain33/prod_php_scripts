1370cba9978be1faa62994cea7db7cb26368fb6f
Manish Jain <man_21jain@yahoo.com>
2022-09-07 12:05:56 +0400

    for correction

--- tracker_catagory.php
+++ tracker_catagory.php
@@ -15,14 +15,13 @@ while ($trackersRow = mysqli_fetch_assoc($trackers_result))
     $trackersData[]=$trackersRow;
   }
 //print_r($trackersData);
-for ($i=0;$i<=count($trackersData);$i++){
-//for ($i=0;$i<=2;$i++){
-  print_r($trackersData[$i]["tid"]);
+//for ($i=0;$i<=count($trackersData);$i++){
+for ($i=0;$i<=2;$i++){
   $result  = $session->execute("select * from trackers_by_trackerid where trackerid=".$trackersData[$i]["tid"]);
   foreach ($result as $row) {
       $catagory[]=$row["foa_type"];
   }
 }
-print_r($catagory);
+print_r($catagory[]);
 //print_r(array_count_values($catagory));
 ?>
