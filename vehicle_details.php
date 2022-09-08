84828282f2ea6b14a82f30a2bc9eaac5ef1c07fd
Manish Jain <man_21jain@yahoo.com>
2022-09-08 09:13:14 +0400

    vehicle details

---
+++ vehicle_details.php
@@ -0,0 +1,29 @@
+<?php
+include('connProd.php') ;
+$username = "dps";
+$password = "dps123";
+$hostname = "172.16.1.4";  
+$dbhandle = mysqli_connect($hostname, $username, $password) ;
+$selected = mysqli_select_db($dbhandle,'dubai') ;
+if($dbhandle->connect_errno > 0){
+  die('Unable to connect to database' . $dbhandle->connect_error);
+}
+$trackers = "select * from tid1";
+$trackers_result = mysqli_query($dbhandle, $trackers);
+while ($trackersRow = mysqli_fetch_assoc($trackers_result))
+  {
+    $trackersData[]=$trackersRow;
+  }
+//print_r($trackersData);
+echo "<table>"
+//for ($i=0;$i<=count($trackersData);$i++){
+for ($i=0;$i<=3;$i++){
+    echo "<tr>"
+  $result  = $session->execute("select * from vehicles_by_vehicleid where trackerid=".$trackersData[$i]["tid"]." allow filtering");
+  foreach ($result as $row) {
+      $chassis[]=$row['chasis_number'];
+      echo "<td>".$trackersData[$i]["tid"]."</td><td>".$row['chasis_number']. "</td></tr>";
+  }
+
+}
+?>
