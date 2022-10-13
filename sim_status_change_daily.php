729023eb7d962de9b72a0c302e30b770251c0ed3
Manish Jain <man_21jain@yahoo.com>
2022-10-13 10:42:55 +0400

    change the condition

--- sim_status_change_daily.php
+++ sim_status_change_daily.php
@@ -1,43 +1,42 @@
 <?php
-// include('connProd.php') ;
-// $options = array('page_size' => 100);
-// $future  = $session->executeAsync("SELECT * FROM trackers_by_imei",$options);
-// $result = $future->get();
+include('connProd.php') ;
+$options = array('page_size' => 100);
+$future  = $session->executeAsync("SELECT * FROM trackers_by_imei",$options);
+$result = $future->get();
 
-// while(true){
-//   //echo "entries in page: " . $result->count() . "<br><br>\n\n";
-//   foreach ($result as $row) {
-//     if($row['trackerid']!=''&& $row['is_deleted']==0 && $row['sim_serial']!=''){
-//       //print_r($row['sim_serial']);
-//       //echo "<br> select * from sim_cards where iccid=".$row['sim_serial'];
-//       $simstatus=$session->execute("select * from sim_cards where iccid='".$row['sim_serial']."'");
-//       foreach ($simstatus as $rows){
-//         // echo "\n";
-//         // print_r($row['sim_serial']);
-//         // echo" - ";
-//         // print_r($rows['status']);
-//         $sim_array[]=$rows;
-//       }
-//     }
-//   }
-//   if ($result->isLastPage()){ break; }
-//   $result=$result->nextPage();
-// }
-// //echo "<br>".count($sim_array)."<br>";
-// //print_r($sim_array[0]);
-// $totsim=$session->execute("select * from sim_cards");
-// foreach ($totsim as $simrows){
-//   $totsimarr[]=$simrows;
-// }
-// //echo "<br>";
-// //print_r($totsim[0]);
-// for ($i=0;$i<=count($totsimarr);$i++){
-//   $msisdn=$totsim[$i]['msisdn'];
-//   //echo $msisdn."<br>/n";
-//   if(array_search($msisdn,$sim_array)){
-//     echo "sim Found /n";
-//   }
-// }
-$a=array("a"=>"red","b"=>"green","c"=>"blue");
-echo array_search("red",$a);
+while(true){
+  //echo "entries in page: " . $result->count() . "<br><br>\n\n";
+  foreach ($result as $row) {
+    if($row['trackerid']!=''&& $row['is_deleted']==0 && $row['sim_serial']!=''){
+      //print_r($row['sim_serial']);
+      //echo "<br> select * from sim_cards where iccid=".$row['sim_serial'];
+      $simstatus=$session->execute("select * from sim_cards where iccid='".$row['sim_serial']."'");
+      foreach ($simstatus as $rows){
+        // echo "\n";
+        // print_r($row['sim_serial']);
+        // echo" - ";
+        // print_r($rows['status']);
+        $sim_array[]=$rows;
+      }
+    }
+  }
+  if ($result->isLastPage()){ break; }
+  $result=$result->nextPage();
+}
+//echo "<br>".count($sim_array)."<br>";
+//print_r($sim_array[0]);
+$totsim=$session->execute("select * from sim_cards");
+foreach ($totsim as $simrows){
+  $totsimarr[]=$simrows;
+}
+//echo "<br>";
+//print_r($totsim[0]);
+for ($i=0;$i<=count($totsimarr);$i++){
+  $msisdn=$totsim[$i]['msisdn'];
+  //echo $msisdn."<br>/n";
+  $searchresult=array_search($msisdn,$sim_array);
+  if($searchresult!== false){
+    echo "sim Found /n";
+  }
+}
 ?>
