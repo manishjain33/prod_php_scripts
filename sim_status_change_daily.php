ac2400f2f7e8b3b6a3bd1a4db18fa32c29a6033c
Manish Jain <man_21jain@yahoo.com>
2022-10-18 13:15:52 +0400

    elseif

--- sim_status_change_daily.php
+++ sim_status_change_daily.php
@@ -22,7 +22,7 @@ foreach ($totsim as $simrows){
   $searchresult=array_search($iccid,$sim_array);
   if($searchresult!== false){
     $simfound[]=$iccid;
-  }else if ($totsimarr[$i]['status']!="inactive"){
+  }elseif ($totsimarr[$i]['status']!="inactive"){
     $update  = $session->execute("UPDATE sim_cards SET status ='inactive' WHERE (iccid = '".$totsimarr[$i]['iccid']."')");
     echo " - updated <br> /n";
   }
