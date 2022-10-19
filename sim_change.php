ba39254757175c24b9c816d4e9ccdc3611ef2fc5
Manish Jain <man_21jain@yahoo.com>
2022-10-19 10:07:19 +0400

    put logs

--- sim_change.php
+++ sim_change.php
@@ -59,7 +59,7 @@ curl_setopt_array($curl, array(
 $response = curl_exec($curl);
 $err= curl_error($curl);
 curl_close($curl);
-//echo $response;
+echo $response;
 $rep=json_decode($response);
 if($rep->error_code!="unauthorized_access"){
     $update  = $session->execute("UPDATE sim_cards SET status = 'active' WHERE (iccid = '".$iccid."')");
