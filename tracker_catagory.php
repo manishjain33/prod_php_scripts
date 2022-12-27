894b959fc3c34ca49ad51fe2fa8f54385dbfa7f9
Manish Jain <man_21jain@yahoo.com>
2022-12-27 10:03:40 +0400

    changes

--- tracker_catagory.php
+++ tracker_catagory.php
@@ -15,13 +15,17 @@ while ($trackersRow = mysqli_fetch_assoc($trackers_result))
     $trackersData[]=$trackersRow;
   }
 //print_r($trackersData);
-//for ($i=0;$i<=count($trackersData);$i++){
-for ($i=0;$i<=4470;$i++){
+echo "<table>";
+for ($i=0;$i<=count($trackersData);$i++){
+//for ($i=0;$i<=4470;$i++){
+  echo "<tr>";
   $result  = $session->execute("select * from trackers_by_trackerid where trackerid=".$trackersData[$i]["tid"]);
   foreach ($result as $row) {
       $catagory[]=$row['foa_type'];
+      echo "<td>".$trackersData[$i]["tid"]."</td><td>".$row['imei']."</td><td>". $row['foa_type']. "</td><td>".$row['tracker_model']."</td></tr>";
   }
 }
+echo "</table>";
 //print_r($catagory);
-print_r(array_count_values($catagory));
+//print_r(array_count_values($catagory));
 ?>
--- vehicle_details.php
+++ vehicle_details.php
@@ -26,4 +26,5 @@ for ($i=0;$i<=count($trackersData);$i++){
   }
 
 }
+echo "</table>";
 ?>
