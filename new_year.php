fa5439cb24c41813c0e935c9d02bfc81bea12f1d
Manish Jain <man_21jain@yahoo.com>
2023-01-01 14:17:10 +0400

    remove logs

--- new_year.php
+++ new_year.php
@@ -7,14 +7,14 @@ $result  = $session->execute("SELECT * FROM new_year_geofence");
 foreach ($result as $row) {
     $trackersData[]=$row;
 }
-print_r($trackersData);
-die();
+//print_r($trackersData);
+//die();
 echo "<table>";
 echo "<tr><td>Trackerid</td><td>Chassis Number</td><td>Category</td></tr>";
 for ($i=0;$i<=count($trackersData);$i++){
 //for ($i=0;$i<=3;$i++){
     echo "<tr>";
-  $result  = $session->execute("select * from vehicles_by_vehicleid where trackerid=".$trackersData[$i]["tid"]." allow filtering");
+  $result  = $session->execute("select * from vehicles_by_vehicleid where trackerid=".$trackersData[$i]." allow filtering");
   foreach ($result as $row) {
       echo "<td>".$trackersData[$i]["tid"]."</td><td>".$row['chasis_number']."</td><td>". $row['category']. "</td><td>".$trackersData[$i]["user"]."</td></tr>";
   }
