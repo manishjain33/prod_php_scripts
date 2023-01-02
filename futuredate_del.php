1f04593c4b40335fdaa437e73b6e3383e4ed096f
Manish Jain <man_21jain@yahoo.com>
2023-01-02 12:49:19 +0400

    logs

--- futuredate_del.php
+++ futuredate_del.php
@@ -4,5 +4,11 @@ $result  = $session->execute("SELECT * FROM last_avl_data WHERE year_month > 202
 foreach ($result as $row) {
     $trackersData[]=$row;
 }
-print_r($trackersData);
+//print_r($trackersData);
+echo "DELETE FROM avl_data WHERE (timestamp = '".$trackersData[0]['timestamp']."') AND (trackerid =".$trackersData[0]['trackerid'] .") AND (year_month =". $trackersData[0]['year_month'].");";
+die();
+for ($i=0;$i<=count($trackersData);$i++){
+    //for ($i=0;$i<=3;$i++){
+    $result  = $session->execute("DELETE FROM avl_data WHERE (timestamp = '".$trackersData[$i]['timestamp']."') AND (trackerid =".$trackersData[$i]['trackerid'] .") AND (year_month =". $trackersData[$i]['year_month'].");");
+}
 ?>
