97edb375ff686f2dc6ce83279cfd2502696a1027
Manish Jain <man_21jain@yahoo.com>
2023-01-02 14:04:01 +0400

    final

--- futuredate_del.php
+++ futuredate_del.php
@@ -5,10 +5,9 @@ foreach ($result as $row) {
     $trackersData[]=$row;
 }
 //print_r($trackersData);
-echo "DELETE FROM avl_data WHERE (timestamp = '".$trackersData[0]['timestamp']."') AND (trackerid =".$trackersData[0]['trackerid'] .") AND (year_month =". $trackersData[0]['year_month'].");";
-die();
 for ($i=0;$i<=count($trackersData);$i++){
     //for ($i=0;$i<=3;$i++){
     $result  = $session->execute("DELETE FROM avl_data WHERE (timestamp = '".$trackersData[$i]['timestamp']."') AND (trackerid =".$trackersData[$i]['trackerid'] .") AND (year_month =". $trackersData[$i]['year_month'].");");
+    echo $trackersData[$i]['trackerid']."- Deleted <br>";
 }
 ?>
