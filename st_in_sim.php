ed8fbf9c7654621ada922dd88a882dc577692d24
Manish Jain <man_21jain@yahoo.com>
2023-01-12 11:13:45 +0400

    add st

--- st_in_sim.php
+++ st_in_sim.php
@@ -3,7 +3,7 @@ include('connProd.php') ;
 $result  = $session->execute("SELECT * FROM trackers_by_imei where is_deleted = 1 ALLOW FILTERING;");
 foreach ($result as $row) {
     $st[]=explode("-",$row["imei"]);
-    $simnumber=$row["sim_number"]."-".$st;
+    $simnumber=$row["sim_number"]."-".$st[1];
     echo $simnumber."<br>/n";
     //$result_imei= $session->execute("update trackers_by_imei set sim_number=");
 
