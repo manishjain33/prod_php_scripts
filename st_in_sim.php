b20e946784d8e95d94ad822f64cee95fdbd34e4c
Manish Jain <man_21jain@yahoo.com>
2023-01-12 11:13:02 +0400

    print sim nuber

--- st_in_sim.php
+++ st_in_sim.php
@@ -2,11 +2,12 @@
 include('connProd.php') ;
 $result  = $session->execute("SELECT * FROM trackers_by_imei where is_deleted = 1 ALLOW FILTERING;");
 foreach ($result as $row) {
-    $trackersData[]=$row;
+    $st[]=explode("-",$row["imei"]);
+    $simnumber=$row["sim_number"]."-".$st;
+    echo $simnumber."<br>/n";
+    //$result_imei= $session->execute("update trackers_by_imei set sim_number=");
+
 }
-//for ($a=0;$a<=count($trackersData);$a++){
-for ($a=0;$a<=1;$a++){
-    $st[]=explode("-",$trackersData[$a]["imei"]);
-}
-print_r($st)
+//print_r($st)
+
 ?>
