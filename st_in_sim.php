d47f06d5eaf02963096170d413d2a634a5d0d675
Manish Jain <man_21jain@yahoo.com>
2023-01-12 11:52:12 +0400

    echo update query

--- st_in_sim.php
+++ st_in_sim.php
@@ -12,6 +12,7 @@ foreach ($result as $row) {
     $result_tid= $session->execute("update trackers_by_trackerid set sim_number='".$simnumber."' where (trackerid =".$row["trackerid"]." );");
     foreach ($row['userid'] as $followed) {
         echo "imei - ".$imei." trackerid - ". $tid . " org - " . $org." userid - ". $followed . " simnumber - ".$simnumber."<br>";
+        echo 'update trackers_by_userid set sim_number ="'.$simnumber.'" where ("orgid" ='.$org.') and ("userid" ='.$followed.') and ("trackerid"='.$tid.')';
         $result_tid= $session->execute('update trackers_by_userid set sim_number ="'.$simnumber.'" where ("orgid" ='.$org.') and ("userid" ='.$followed.') and ("trackerid"='.$tid.')');
     }
     print_r($row["imei"]);
