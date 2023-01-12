cce2460b8e778a899c0b306fe6b6e2944c0df986
Manish Jain <man_21jain@yahoo.com>
2023-01-12 12:15:23 +0400

    echo

--- st_in_sim.php
+++ st_in_sim.php
@@ -6,10 +6,11 @@ foreach ($result as $row) {
     $tid= $row['trackerid'];
     $org= $row['orgid'];
     $st=explode("-",$imei);
-    print_r($st[1]);
-    echo"<br>";
+    //print_r($st[1]);
     $simnumber=$row["sim_number"]."-".$st[1];
     echo "imei - ".$imei." trackerid - ". $tid . " org - " . $org." userid - ". $followed . " simnumber - ".$simnumber."<br>/n";
+    echo 'update trackers_by_userid set sim_number ="'.$simnumber.'" where ("orgid" ='.$org.') and ("userid" ='.$followed.') and ("trackerid"='.$tid.')';
+    echo "<br>";
     // $result_imei= $session->execute("update trackers_by_imei set sim_number='".$simnumber."' where (imei='".$row["imei"]."')");
     // $result_tid= $session->execute("update trackers_by_trackerid set sim_number='".$simnumber."' where (trackerid =".$row["trackerid"]." );");
     // foreach ($row['userid'] as $followed) {
@@ -20,5 +21,6 @@ foreach ($result as $row) {
 
 }
 //print_r($st)
+//UPDATE "earthone"."trackers_by_userid" SET "sim_number" = '971564076295-ST1671015399455' WHERE ("orgid" = e99f7860-35e7-11e8-8562-3fa9eb090547) AND ("trackerid" = fb0de730-35e7-11e8-b7c2-d5d197cd5ddc) AND ("userid" = ea0b0b70-35e7-11e8-8db2-0c33bef12861);
 
 ?>
