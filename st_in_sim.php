dddbf59ab6111d2b4e9751eaabb5999e0f99e52c
Manish Jain <man_21jain@yahoo.com>
2023-01-12 11:33:37 +0400

    update query

--- st_in_sim.php
+++ st_in_sim.php
@@ -4,9 +4,14 @@ $result  = $session->execute("SELECT * FROM trackers_by_imei where is_deleted = 
 foreach ($result as $row) {
     $st=explode("-",$row["imei"]);
     $simnumber=$row["sim_number"]."-".$st[1];
-    echo $simnumber."<br>/n";
-    //$result_imei= $session->execute("update trackers_by_imei set sim_number=");
-
+    //echo $simnumber."<br>/n";
+    $result_imei= $session->execute("update trackers_by_imei set sim_number='".$simnumber."' where (imei='".$row["imei"]."')");
+    $result_tid= $session->execute("update trackers_by_trackerid set sim_number='".$simnumber."' where (trackerid =".$row["trackerid"]." );");
+    foreach ($row['userid'] as $followed) {
+        $result_tid= $session->execute("update trackers_by_userid set sim_number='".$simnumber."' where ('orgid' =".$row["orgid"].") and ('userid' =".$followed.") and ('trackerid'=".$row["trackerid"].")");
+    }
+    print_r($row["imei"]);
+    die();
 }
 //print_r($st)
 
