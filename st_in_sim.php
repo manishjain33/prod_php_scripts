161618c33c671c33fbfa3569b8953a1004b81746
Manish Jain <man_21jain@yahoo.com>
2023-01-12 11:37:16 +0400

    chage query

--- st_in_sim.php
+++ st_in_sim.php
@@ -2,13 +2,16 @@
 include('connProd.php') ;
 $result  = $session->execute("SELECT * FROM trackers_by_imei where is_deleted = 1 ALLOW FILTERING;");
 foreach ($result as $row) {
+    $imei= $row['imei'];
+    $tid= $row['trackerid'];
+    $org= $row['orgid'];
     $st=explode("-",$row["imei"]);
     $simnumber=$row["sim_number"]."-".$st[1];
     //echo $simnumber."<br>/n";
     $result_imei= $session->execute("update trackers_by_imei set sim_number='".$simnumber."' where (imei='".$row["imei"]."')");
     $result_tid= $session->execute("update trackers_by_trackerid set sim_number='".$simnumber."' where (trackerid =".$row["trackerid"]." );");
     foreach ($row['userid'] as $followed) {
-        $result_tid= $session->execute("update trackers_by_userid set sim_number='".$simnumber."' where ('orgid' =".$row["orgid"].") and ('userid' =".$followed.") and ('trackerid'=".$row["trackerid"].")");
+        $result_tid= $session->execute('update trackers_by_userid set "sim_number"="'.$simnumber.'" where ("orgid" ='.$org.') and ("userid" ='.$followed.') and ("trackerid"='.$tid.')');
     }
     print_r($row["imei"]);
     die();
