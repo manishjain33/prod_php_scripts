376f50ea49b857a2301bc69f7437d33859c2d804
Manish Jain <man_21jain@yahoo.com>
2023-02-08 15:50:23 +0400

    for removed

--- iccid_blank.php
+++ iccid_blank.php
@@ -14,8 +14,8 @@ while ($trackersRow = mysqli_fetch_assoc($trackers_result))
   {
     $trackersData[]=$trackersRow;
   }
-for ($b=0;$b<=3;$b++){
-//for ($b=0;$b<=count($trackersData);$b++){
+//for ($b=0;$b<=3;$b++){
+for ($b=0;$b<=count($trackersData);$b++){
     $result  = $session->execute("SELECT * FROM trackers_by_imei where imei ='".$trackersData[$b]['imei']."' ");
     foreach ($result as $row){
         $imei=$trackersData[$b]['imei'];
