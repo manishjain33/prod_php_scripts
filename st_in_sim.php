b1d3c3e8263ac70a30add4d4f35ae1d07c3c6e09
Manish Jain <man_21jain@yahoo.com>
2023-02-14 11:32:23 +0400

    sim serial st update

--- st_in_sim.php
+++ st_in_sim.php
@@ -7,12 +7,12 @@ foreach ($result as $row) {
     $org= $row['orgid'];
     $st=explode("-",$imei);
     //print_r($st[1]);
-    $simnumber=$row["sim_number"]."-".$st[1];
-    $result_imei= $session->execute("update trackers_by_imei set sim_number='".$simnumber."' where (imei='".$row["imei"]."')");
-    $result_tid= $session->execute("update trackers_by_trackerid set sim_number='".$simnumber."' where (trackerid =".$row["trackerid"]." );");
+    $simserial=$row["sim_serial"]."-".$st[1];
+    $result_imei= $session->execute("update trackers_by_imei set sim_serial='".$simserial."' where (imei='".$row["imei"]."')");
+    $result_tid= $session->execute("update trackers_by_trackerid set sim_serial='".$simserial."' where (trackerid =".$row["trackerid"]." );");
     foreach ($row['userid'] as $followed) {
-        echo "imei - ".$imei." trackerid - ". $tid . " org - " . $org." userid - ". $followed . " simnumber - ".$simnumber."<br><br>";
-        $result_tid= $session->execute("update trackers_by_userid set sim_number ='".$simnumber."' where (orgid =".$org.") and (userid =".$followed.") and (trackerid=".$tid.")");
+        echo "imei - ".$imei." trackerid - ". $tid . " org - " . $org." userid - ". $followed . " simserial - ".$simserial."<br><br>";
+        $result_tid= $session->execute("update trackers_by_userid set sim_serial ='".$simserial."' where (orgid =".$org.") and (userid =".$followed.") and (trackerid=".$tid.")");
     }
 }
 //print_r($st)
