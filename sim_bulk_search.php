ac77083faa324027debb08d7ba4a7e3f1b589ccb
Manish Jain <man_21jain@yahoo.com>
2023-03-28 13:06:24 +0400

    proper logs

--- sim_bulk_search.php
+++ sim_bulk_search.php
@@ -1,7 +1,7 @@
 <?php
 include('connProd.php') ;
 $sim[]=array("971831100498322", "971831100444974");
-for ($i=0;$i<=count($sim[0]);$i++){
+for ($i=0;$i<=(count($sim[0])-1);$i++){
     echo "SELECT * FROM trackers_by_imei where sim_number='".$sim[0][$i]."' ALLOW FILTERING";
     $result  = $session->execute("SELECT * FROM trackers_by_imei where sim_number='".$sim[0][$i]."' ALLOW FILTERING");
     foreach ($result as $row) {
