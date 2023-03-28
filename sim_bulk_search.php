0d1be1b1a08a582a20d2d9334728d2f16c194392
Manish Jain <man_21jain@yahoo.com>
2023-03-28 13:04:51 +0400

    put logs

--- sim_bulk_search.php
+++ sim_bulk_search.php
@@ -2,6 +2,7 @@
 include('connProd.php') ;
 $sim[]=array("971831100498322", "971831100444974");
 for ($i=0;$i<=count($sim[0]);$i++){
+    echo "SELECT * FROM trackers_by_imei where sim_number='".$sim[0][$i]."' ALLOW FILTERING";
     $result  = $session->execute("SELECT * FROM trackers_by_imei where sim_number='".$sim[0][$i]."' ALLOW FILTERING");
     foreach ($result as $row) {
         echo $row['imei']." , ";
