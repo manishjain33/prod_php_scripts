502448b77573eb0de54dc908aedb52d31e492ea6
Manish Jain <man_21jain@yahoo.com>
2023-04-25 15:57:14 +0400

    all commented

--- geocoding.php
+++ geocoding.php
@@ -1,22 +1,22 @@
 <?php
-$username = "dps";
-$password = "dps123";
-$hostname = "172.16.1.4"; 
+// $username = "dps";
+// $password = "dps123";
+// $hostname = "172.16.1.4"; 
 
-//connection to the database
-$dbhandle = mysqli_connect($hostname, $username, $password) ;
-$selected = mysqli_select_db($dbhandle,"dubai") ;
-if($dbhandle->connect_errno > 0)
-    {
-        die('Unable to connect to database' . $dbhandle->connect_error);
-    }
-$sql = "select * from citv";
-$result = mysqli_query($dbhandle,$sql);
-while ($trackerid = mysqli_fetch_assoc($result))
-{
-  $tidData[]=$trackerid;
-}
-//echo count($tidData);
+// //connection to the database
+// $dbhandle = mysqli_connect($hostname, $username, $password) ;
+// $selected = mysqli_select_db($dbhandle,"dubai") ;
+// if($dbhandle->connect_errno > 0)
+//     {
+//         die('Unable to connect to database' . $dbhandle->connect_error);
+//     }
+// $sql = "select * from citv";
+// $result = mysqli_query($dbhandle,$sql);
+// while ($trackerid = mysqli_fetch_assoc($result))
+// {
+//   $tidData[]=$trackerid;
+// }
+// //echo count($tidData);
 $postraw=array("coords"=>array("trackerid"=>"23bfbe40-f6f1-11e8-8593-db6ca2a126d5","lat"=>"24.50086021423340","lng"=>"54.39120864868164"));
 echo json_encode($postraw);
 ?>
