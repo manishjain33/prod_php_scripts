9caa97a912ce4b060978b9842377a3e3a9c30234
Manish Jain <man_21jain@yahoo.com>
2023-04-27 16:46:28 +0400

    logs

--- avl_data_pulling.php
+++ avl_data_pulling.php
@@ -4,6 +4,7 @@ $result  = $session->execute("SELECT trackerid FROM trackers_by_trackerid");
     foreach ($result as $row) {
         $tid[]=$row;
     }
+echo count($tid);
 for ($a=0,$a<1;$a++;){
     $resultAVL  = $session->execute("SELECT * from avl_data WHERE year_month=202304 and trackerid =".$tid[$a]." and timestamp >=1682334000000  and timestamp <= 1682334300000");
     foreach($resultAVL as $avl){
