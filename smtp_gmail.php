29bb2740f6eccf9028b777fd2a57d94588093c62
Manish Jain <man_21jain@yahoo.com>
2023-07-07 21:01:55 +0400

    change user

--- smtp_gmail.php
+++ smtp_gmail.php
@@ -17,8 +17,8 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'donotreply@boxdrop.ae';                     //SMTP username
-    $mail->Password   = 'BG5gkQ56#9';                               //SMTP password
+    $mail->Username   = 'maan.jain@gmail.com';                     //SMTP username
+    $mail->Password   = 'monu1030';                               //SMTP password
     $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
     $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
