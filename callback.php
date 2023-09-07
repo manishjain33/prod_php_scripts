5bed7ce3fb3598232c4ab5f49d80762e869cd7fa
Manish Jain <man_21jain@yahoo.com>
2023-09-07 16:37:01 +0400

    put

--- callback.php
+++ callback.php
@@ -1,8 +1,9 @@
 <?php
+$test=$_POST;
 $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
 $txt = "John Doe\n";
 fwrite($myfile, $txt);
 $txt = "Jane Doe\n";
-fwrite($myfile, $txt);
+fwrite($myfile, $test);
 fclose($myfile);
 ?>
