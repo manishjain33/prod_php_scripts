18f7a9e0c4b175b53e5cf2a1c6807101517ad46f
Manish Jain <man_21jain@yahoo.com>
2024-02-05 20:57:11 +0400

    echo url

--- boxdrop_sms.php
+++ boxdrop_sms.php
@@ -14,14 +14,14 @@ while ($trackersRow = mysqli_fetch_assoc($trackers_result))
     $trackersData[]=$trackersRow;
   }
   $sleep=10;
-//for ($a=0;$a<=1;$a++){
-for ($a=0;$a<=count($trackersData);$a++){
+for ($a=0;$a<=1;$a++){
+//for ($a=0;$a<=count($trackersData);$a++){
     $msg="BoxDrop Notice: From 7th Feb 2024, BoxDrop services will be subject to charges. For top-up instructions, please visit: https://boxdrop.ae/doc/BDpay_v1.pdf .";
     $msg=urlencode($msg);
     //$url="http://mshastra.com/sendurl.aspx?user=20093058&pwd=Emsira$058&senderid=SIRAPROMO&mobileno=971552254640&msgtext=".$msg."&priority=High&CountryCode=971";
-    $url="http://mshastra.com/sendurl.aspx?user=20093058&pwd=Emsira$058&senderid=SIRAPROMO&mobileno=".$trackersData[$a]["phone"]."&msgtext=".$msg."&priority=High&CountryCode=971";
+    echo $url="http://mshastra.com/sendurl.aspx?user=20093058&pwd=Emsira$058&senderid=SIRAPROMO&mobileno=".$trackersData[$a]["phone"]."&msgtext=".$msg."&priority=High&CountryCode=971";
     $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
     $curl_scraped_page = curl_exec($ch);
     curl_close($ch);
     echo $curl_scraped_page."<br>";
