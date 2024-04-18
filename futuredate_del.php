8d1acef11fe98dbacf7869b108dee53261cb91d4
Manish Jain <man_21jain@yahoo.com>
2024-04-18 10:27:09 +0400

    change dagte

--- futuredate_del.php
+++ futuredate_del.php
@@ -1,6 +1,6 @@
 <?php
 include('connProd.php') ;
-$result  = $session->execute("SELECT * FROM last_avl_data WHERE year_month > 202401 ALLOW FILTERING;");
+$result  = $session->execute("SELECT * FROM last_avl_data WHERE year_month > 202404 ALLOW FILTERING;");
 foreach ($result as $row) {
     $trackersData[]=$row;
 }
