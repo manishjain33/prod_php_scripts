975ff4936eaae4d1947943b549f409033662f284
Manish Jain <man_21jain@yahoo.com>
2024-06-07 10:14:42 +0400

    allow filering

--- bulk_stop_service.php
+++ bulk_stop_service.php
@@ -6,7 +6,6 @@ $data=json_decode($rawdata);
 for($a=0;$a<=count($data->chassis);$a++){
      echo "chassis - ".$data->chassis[$a]."<br>";
     // print_r($sim_array[$a]);
-    echo "select trackerid from vehicles_by_vehicleid where chassis=".$data->chassis[$a]." allow filtring";
-    //echo $tid  = $session->execute("select trackerid from vehicles_by_vehicleid where chassis=".$data->chassis[$a]." allow filtring");
+    echo $tid  = $session->execute("select trackerid from vehicles_by_vehicleid where chasis_number='".$data->chassis[$a]."' allow filtering");
     //echo $tid;
 }
