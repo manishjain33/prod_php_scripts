915e7b3befa60e6d65a202480846585467da7eff
Manish Jain <man_21jain@yahoo.com>
2024-06-07 10:20:42 +0400

    change variable

--- bulk_stop_service.php
+++ bulk_stop_service.php
@@ -6,7 +6,7 @@ $data=json_decode($rawdata);
 for($a=0;$a<=count($data->chassis);$a++){
      //echo "chassis - ".$data->chassis[$a]."<br>";
     // print_r($sim_array[$a]);
-    $tid  = $session->execute("select trackerid from vehicles_by_vehicleid where chasis_number='".$data->chassis[$a]."' allow filtering");
+    $result  = $session->execute("select trackerid from vehicles_by_vehicleid where chasis_number='".$data->chassis[$a]."' allow filtering");
     foreach ($result as $row) {
         echo $row['trackerid'].",";
     }
