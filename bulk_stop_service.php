0fb704ffbdb64f2132db2bc8ae91dc045c699a29
Manish Jain <man_21jain@yahoo.com>
2024-06-07 09:26:42 +0400

    print

--- bulk_stop_service.php
+++ bulk_stop_service.php
@@ -2,4 +2,6 @@
 include('connProd.php') ;
 $rawdata = file_get_contents("php://input");
 $data=json_decode($rawdata);
-print_r($data);
+print_r($data->chassis[0]) ;
+echo "with out r";
+echo $data->chassis[0];
