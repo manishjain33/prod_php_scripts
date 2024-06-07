978138b65476b7e0667ad42795d2ef7258092647
Manish Jain <man_21jain@yahoo.com>
2024-06-07 09:29:38 +0400

    echo chassis

--- bulk_stop_service.php
+++ bulk_stop_service.php
@@ -2,6 +2,10 @@
 include('connProd.php') ;
 $rawdata = file_get_contents("php://input");
 $data=json_decode($rawdata);
-print_r($data->chassis[0]) ;
-echo "with out r";
-echo $data->chassis[0];
+#print_r($data->chassis[0]) ;
+for($a=0;$a<=count($data->chassis);$a++){
+     echo "chassis - ".$data->chassis[$a]."<br>";
+    // print_r($sim_array[$a]);
+    // $update  = $session->execute("UPDATE sim_cards SET status ='active' WHERE (iccid = '".$sim_array[$a]."')");
+    // echo " - updated <br> \n";
+}
