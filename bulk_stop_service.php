a1e819ad75910eec6481b55c0d18047d1a05a7b6
Manish Jain <man_21jain@yahoo.com>
2024-06-07 10:19:46 +0400

    print difference

--- bulk_stop_service.php
+++ bulk_stop_service.php
@@ -7,5 +7,7 @@ for($a=0;$a<=count($data->chassis);$a++){
      //echo "chassis - ".$data->chassis[$a]."<br>";
     // print_r($sim_array[$a]);
     $tid  = $session->execute("select trackerid from vehicles_by_vehicleid where chasis_number='".$data->chassis[$a]."' allow filtering");
-    echo $tid.",";
+    foreach ($result as $row) {
+        echo $row['trackerid'].",";
+    }
 }
