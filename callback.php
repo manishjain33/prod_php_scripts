474ec411b73a3a536b0b597cdaf825950849d913
Manish Jain <man_21jain@yahoo.com>
2024-06-26 13:24:11 +0400

    print

--- callback.php
+++ callback.php
@@ -1,8 +1,7 @@
 <?php
-for ($i = 0; $i < 1000; $i++) {
 $test=$_POST;
+print_r( $test );
 $myfile = fopen("newfile.txt", "a") or die("Unable to open file!");
 fwrite($myfile, json_encode($test));
 fclose($myfile);
-}
 ?>
