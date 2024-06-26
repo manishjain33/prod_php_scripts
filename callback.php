9960b3a9a9e828c864d492035332c7afe723407e
Manish Jain <man_21jain@yahoo.com>
2024-06-26 13:17:18 +0400

    put for loop

--- callback.php
+++ callback.php
@@ -1,6 +1,8 @@
 <?php
+for ($i = 0; $i < 1000; $i++) {
 $test=$_POST;
 $myfile = fopen("newfile.txt", "a") or die("Unable to open file!");
 fwrite($myfile, json_encode($test));
 fclose($myfile);
+}
 ?>
