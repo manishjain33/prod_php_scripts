e7b5eaf22638a116ba66aa43c7c1d14ea14c4411
Manish Jain <man_21jain@yahoo.com>
2024-08-22 14:18:16 +0400

    field change

--- tars_comp.php
+++ tars_comp.php
@@ -8,12 +8,12 @@ $selected = mysqli_select_db($dbhandle,'tars_vin') ;
 if($dbhandle->connect_errno > 0){
   die('Unable to connect to database' . $dbhandle->connect_error);
 }
-$vin_tars = "select * from vin";
-$vin_tars_result = mysqli_query($dbhandle, $vin_tars);
-while ($vin_tarsRow = mysqli_fetch_assoc($vin_tars_result))
-  {
-    $vin_tarsData[]=$vin_tarsRow;
-  }
-$result  = $session->execute("select * from vehicles_by_vehicleid where chassis='JM7GL4S31P147738' allow filtering");
+// $vin_tars = "select * from vin";
+// $vin_tars_result = mysqli_query($dbhandle, $vin_tars);
+// while ($vin_tarsRow = mysqli_fetch_assoc($vin_tars_result))
+//   {
+//     $vin_tarsData[]=$vin_tarsRow;
+//   }
+$result  = $session->execute("select * from vehicles_by_vehicleid where chasis_number='JM7GL4S31P147738' allow filtering");
 //$result  = $session->execute("select * from vehicles_by_vehicleid where chassis='".sdfsdfsdfsdfsd."' allow filtering");
 var_dump($result);
