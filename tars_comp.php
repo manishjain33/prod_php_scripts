d742469dad4cec54b6ab90b9d187b9a2f1380489
Manish Jain <man_21jain@yahoo.com>
2024-09-20 09:40:41 +0400

    db name change

--- tars_comp.php
+++ tars_comp.php
@@ -2,20 +2,20 @@
 include ('connProd.php') ;
 $username = "dps";
 $password = "dps123";
-$hostname = "172.16.1.4";  
+$hostname = "localhost";  
 $dbhandle = mysqli_connect($hostname, $username, $password) ;
-$selected = mysqli_select_db($dbhandle,'tars_vin') ;
+$selected = mysqli_select_db($dbhandle,'tars') ;
 if($dbhandle->connect_errno > 0){
   die('Unable to connect to database' . $dbhandle->connect_error);
 }
-$vin_tars = "select * from vin";
+$vin_tars = "select * from comp_transit";
 $vin_tars_result = mysqli_query($dbhandle, $vin_tars);
 while ($vin_tarsRow = mysqli_fetch_assoc($vin_tars_result))
   {
     $vin_tarsData[]=$vin_tarsRow;
   }
-//for($a=0;$a<4;$a++){
-for($a=0;$a<count($vin_tarsData);$a++){
+for($a=0;$a<4;$a++){
+//for($a=0;$a<count($vin_tarsData);$a++){
     $result  = $session->execute("select * from vehicles_by_vehicleid where chasis_number='".$vin_tarsData[$a]["vin"]."' allow filtering");
     foreach ($result as $row) {
         if($row['category']==null){
