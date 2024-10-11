435b2b8f594d8afbf37712d630cd45efb344b653
Manish Jain <man_21jain@yahoo.com>
2024-10-11 09:32:07 +0400

    with condition

--- vendor_list.php
+++ vendor_list.php
@@ -7,10 +7,18 @@ $selected = mysqli_select_db($dbhandle,'vendor') ;
 if($dbhandle->connect_errno > 0){
   die('Unable to connect to database' . $dbhandle->connect_error);
 }
-echo $vendors = "select * from vendor_list where emirates= '".$_GET["em"]."'";
+if($_GET["em"]=="du"){
+    $vendors = "select * from vendor_list where emirates= 'Dubai'";
+}else if($_GET["eu"]=="sh"){
+    $vendors = "select * from vendor_list where emirates= 'Sharjah'";
+}else if($_GET["eu"]=="fu"){
+    $vendors = "select * from vendor_list where emirates= 'Fajairah'";
+}else if($_GET["eu"]=="all"){
+    $vendors = "select * from vendor_list";
+}
 $vendors_result = mysqli_query($dbhandle, $vendors);
 while ($vendorsRow = mysqli_fetch_assoc($vendors_result))
   {
     $vendorsData[]=$vendorsRow;
   }
-  print_r($vendorsData);
+  //print_r($vendorsData);
