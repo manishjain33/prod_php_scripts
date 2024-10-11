9b277a9fdcb4f9e7a5d05332435164c1b6c4b202
Manish Jain <man_21jain@yahoo.com>
2024-10-11 10:13:09 +0400

    tras file chagnes

--- tars_comp.php
+++ tars_comp.php
@@ -8,7 +8,7 @@ $selected = mysqli_select_db($dbhandle,'tars') ;
 if($dbhandle->connect_errno > 0){
   die('Unable to connect to database' . $dbhandle->connect_error);
 }
-$vin_tars = "select * from comp_transit";
+$vin_tars = "select * from RAC_comp";
 $vin_tars_result = mysqli_query($dbhandle, $vin_tars);
 while ($vin_tarsRow = mysqli_fetch_assoc($vin_tars_result))
   {
@@ -16,14 +16,14 @@ while ($vin_tarsRow = mysqli_fetch_assoc($vin_tars_result))
   }
 //for($a=0;$a<4;$a++){
 for($a=0;$a<count($vin_tarsData);$a++){
-    $result  = $session->execute("select * from vehicles_by_vehicleid where chasis_number='".$vin_tarsData[$a]["vin"]."' allow filtering");
+    $result  = $session->execute("select * from vehicles_by_vehicleid where chasis_number='".$vin_tarsData[$a]["chassis"]."' allow filtering");
     foreach ($result as $row) {
         if($row['category']==null){
-            $vin_tars_update = "UPDATE comp_transit SET remark =  'not_exist' WHERE sno =".$vin_tarsData[$a]["sno"];
+            $vin_tars_update = "UPDATE RAC_comp SET remark =  'not_exist' WHERE sno =".$vin_tarsData[$a]["sno"];
             $vin_tars_result_update = mysqli_query($dbhandle, $vin_tars_update);
         }
         if($row['category']!=null){
-            $vin_tars_update = "UPDATE comp_transit SET remark =  'exist' WHERE sno =".$vin_tarsData[$a]["sno"];
+            $vin_tars_update = "UPDATE RAC_comp SET remark =  'exist' WHERE sno =".$vin_tarsData[$a]["sno"];
             $vin_tars_result_update = mysqli_query($dbhandle, $vin_tars_update);
         } 
     }
--- vendor_list.php
+++ vendor_list.php
@@ -22,4 +22,4 @@ while ($vendorsRow = mysqli_fetch_assoc($vendors_result))
     $vendorsData[]=$vendorsRow;
   }
   //print_r($vendorsData);
-echo json_encode($vendorsData[]);
+echo json_encode($vendorsData);
