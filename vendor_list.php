66330f7815f11ec3501656e53f276f08695d9be6
Manish Jain <man_21jain@yahoo.com>
2024-10-11 11:09:34 +0400

    gpt code

--- vendor_list.php
+++ vendor_list.php
@@ -2,30 +2,56 @@
 $username = "dps";
 $password = "dps123";
 $hostname = "localhost";  
-$dbhandle = mysqli_connect($hostname, $username, $password) ;
-$selected = mysqli_select_db($dbhandle,'vendor') ;
-if($dbhandle->connect_errno > 0){
-  die('Unable to connect to database' . $dbhandle->connect_error);
+$dbhandle = mysqli_connect($hostname, $username, $password);
+
+// Check if connection was successful
+if(!$dbhandle){
+  die('Unable to connect to database: ' . mysqli_connect_error());
 }
-$_GET["em"]="all";
-switch ($_GET["em"]) {
+
+// Select the database
+$selected = mysqli_select_db($dbhandle, 'vendor');
+if(!$selected){
+  die('Unable to select database: ' . mysqli_error($dbhandle));
+}
+
+// Check if "em" parameter is set in the URL, else default to "all"
+$emirate = isset($_GET["em"]) ? $_GET["em"] : "all";
+
+// Prepare the query based on the "em" value
+switch ($emirate) {
   case "du":
-    $vendors = "select * from vendor_list where emirates= 'Dubai'";
+    $vendors = "SELECT * FROM vendor_list WHERE emirates = 'Dubai'";
     break;
   case "sh":
-    $vendors = "select * from vendor_list where emirates= 'Sharjah'";
+    $vendors = "SELECT * FROM vendor_list WHERE emirates = 'Sharjah'";
     break;
   case "fu":
-    $vendors = "select * from vendor_list where emirates= 'Fajairah'";
+    $vendors = "SELECT * FROM vendor_list WHERE emirates = 'Fajairah'";
     break;
   case "all":
-    $vendors = "select * from vendor_list";
+  default:
+    $vendors = "SELECT * FROM vendor_list";
     break;
 }
+
+// Execute the query and check for errors
 $vendors_result = mysqli_query($dbhandle, $vendors);
-while ($vendorsRow = mysqli_fetch_assoc($vendors_result))
-  {
-    $vendorsData[]=$vendorsRow;
-  }
-  //print_r($vendorsData);
-  echo $finaldata=json_encode($vendorsData,JSON_UNESCAPED_UNICODE);
+if (!$vendors_result) {
+  die('Error in query: ' . mysqli_error($dbhandle));
+}
+
+// Initialize an array to store vendor data
+$vendorsData = [];
+
+// Fetch the results into an array
+while ($vendorsRow = mysqli_fetch_assoc($vendors_result)) {
+  $vendorsData[] = $vendorsRow;
+}
+
+// Output the data as JSON
+echo json_encode($vendorsData, JSON_UNESCAPED_UNICODE);
+
+// Close the database connection
+mysqli_close($dbhandle);
+?>
